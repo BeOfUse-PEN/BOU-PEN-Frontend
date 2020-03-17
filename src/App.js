545f2afdb0f138c0619c89f601a825274e60c1fc
Mayank_MP5 <mayank5pande@gmail.com>
2020-03-17 13:24:11 +0530

    Folder Structure Optimisations Added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home  from './components/Home';
-import Blogs from './components/Blogs';
-import SignIn  from './components/SignIn';
-import Contact  from './components/Contact';
-import Navbar from "./components/navbar";
-import CourseSpecification from "./components/CourseSpecification";
-import Footer from "./components/Footer"
+import Home  from './components/home/Home';
+import Blogs from './components/home/Blogs';
+import SignIn  from './components/home/SignIn';
+import Contact  from './components/home/Contact';
+import CourseSpecification from "./components/home/CourseSpecification";
+
+import Navbar from "./components/comman/navbar";
+import Footer from "./components/comman/Footer";
 
 function App() {
   return (
     <React.Fragment>
     <Router>
     <Navbar/>
-    
-    <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/Blogs" component={Blogs} />
-    <Route path="/signIn" exact component={SignIn} />
-    <Route path="/Contact" exact component={Contact} />
-    <Route path="/CourseSpecification" exact component={CourseSpecification} />
-    <Route path="/home" component={Home} />
-    
-
-  </Switch>
+   <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/Blogs" component={Blogs} />
+      <Route path="/signIn" exact component={SignIn} />
+      <Route path="/Contact" exact component={Contact} />
+      <Route path="/CourseSpecification" exact component={CourseSpecification} />
+      <Route path="/home" component={Home} />
+   </Switch>
     <Footer/>
     </Router>
     </React.Fragment>
--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
-import bg from '../assets/bg.jpg';
+import bg from '../../assets/bg.jpg';
 
 const Styles = styled.div`
   .jumbo {
