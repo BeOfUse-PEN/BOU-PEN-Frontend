e65b1f3c18493adbf987378ea761fee6efe477bf
Mayank_MP5 <mayank5pande@gmail.com>
2020-03-17 13:05:52 +0530

    Existing Files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home  from './components/Home';
 import Blogs from './components/Blogs';
